bbe65de7d18217f5cc105b608ed133f1afb67bc3
Juan Peri <eternoperegrino@gmail.com>
2026-09-04 14:24:33 +0200

    Add horizontal detent scrolling

--- crosses-detent/src/pointing/input_processor_scroll_detent.c
+++ crosses-detent/src/pointing/input_processor_scroll_detent.c
@@ -19,11 +19,13 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 struct scroll_detent_config {
     uint8_t type;
+    size_t codes_len;
+    const uint16_t *codes;
     uint16_t threshold;
 };
 
 struct scroll_detent_data {
-    int32_t accumulator;
+    int32_t accumulators[2];
 };
 
 static int scroll_detent_handle_event(const struct device *dev, struct input_event *event,
@@ -36,22 +38,36 @@ static int scroll_detent_handle_event(const struct device *dev, struct input_eve
     ARG_UNUSED(param2);
     ARG_UNUSED(state);
 
-    if (event->type != config->type || event->code != INPUT_REL_WHEEL) {
+    if (event->type != config->type) {
         return ZMK_INPUT_PROC_CONTINUE;
     }
 
-    data->accumulator += event->value;
-    event->value = 0;
-
-    if (data->accumulator >= config->threshold) {
-        event->value = 1;
-        data->accumulator -= config->threshold;
-    } else if (data->accumulator <= -config->threshold) {
-        event->value = -1;
-        data->accumulator += config->threshold;
+    size_t code_idx = config->codes_len;
+    for (size_t i = 0; i < config->codes_len; i++) {
+        if (event->code == config->codes[i]) {
+            code_idx = i;
+            break;
+        }
     }
 
-    LOG_DBG("scroll detent accumulator %d, output %d", data->accumulator, event->value);
+    if (code_idx >= ARRAY_SIZE(data->accumulators)) {
+        return ZMK_INPUT_PROC_CONTINUE;
+    }
+
+    int32_t *accumulator = &data->accumulators[code_idx];
+    *accumulator += event->value;
+    event->value = 0;
+
+    if (*accumulator >= config->threshold) {
+        event->value = 1;
+        *accumulator -= config->threshold;
+    } else if (*accumulator <= -config->threshold) {
+        event->value = -1;
+        *accumulator += config->threshold;
+    }
+
+    LOG_DBG("scroll detent code %d accumulator %d, output %d", event->code, *accumulator,
+            event->value);
     return ZMK_INPUT_PROC_CONTINUE;
 }
 
@@ -63,8 +79,11 @@ static const struct zmk_input_processor_driver_api scroll_detent_driver_api = {
     static struct scroll_detent_data scroll_detent_data_##n;                                       \
     static const struct scroll_detent_config scroll_detent_config_##n = {                         \
         .type = DT_INST_PROP_OR(n, type, INPUT_EV_REL),                                           \
+        .codes_len = DT_INST_PROP_LEN(n, codes),                                                   \
+        .codes = DT_INST_PROP(n, codes),                                                           \
         .threshold = DT_INST_PROP(n, threshold),                                                   \
     };                                                                                             \
+    BUILD_ASSERT(DT_INST_PROP_LEN(n, codes) <= 2, "scroll detent supports at most two codes");    \
     BUILD_ASSERT(DT_INST_PROP(n, threshold) > 0, "scroll detent threshold must be positive");     \
     DEVICE_DT_INST_DEFINE(n, NULL, NULL, &scroll_detent_data_##n,                                  \
                           &scroll_detent_config_##n, POST_KERNEL,                                \
